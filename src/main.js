51fc8895fe295ade6cde655224d282c45a5ad003
anoju <zun5761@gmail.com>
2024-03-27 11:13:06 +0900

    anime 라이브러리 추가

--- src/main.js
+++ src/main.js
@@ -1,11 +1,14 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from '@/router/index';
-import { registerComponents } from '@/components/index';
+const app = createApp(App);
 
+// 스타일 적용
 import './assets/scss/front.scss';
 
-const app = createApp(App);
+// Component 등록
+import { registerComponents } from '@/components/index';
+registerComponents(app);
 
 // swiper
 import { Swiper, SwiperSlide } from 'swiper/vue';
@@ -15,6 +18,9 @@ import 'swiper/css';
 import 'swiper/css/pagination';
 import 'swiper/css/navigation';
 
-registerComponents(app);
+// anime
+import anime from 'animejs';
+// app.config.globalProperties.$anime = anime;
+app.provide('$anime', anime);
 
 app.use(router).mount('#app');
--- src/views/main/index.vue
+++ src/views/main/index.vue
@@ -1,10 +1,51 @@
 <template>
-  <img alt="Vue logo" src="@/assets/images/logo.png" />
+  <img ref="logoImg" alt="Vue logo" src="@/assets/images/logo.png" @click="imgMove()" />
   <HelloWorld msg="Welcome to Your Vue.js App" />
 </template>
+<script setup>
+import { ref, inject } from 'vue';
+//import { ref, getCurrentInstance } from 'vue';
+//const { $anime } = getCurrentInstance().appContext.config.globalProperties;
+const $anime = inject('$anime');
+const logoImg = ref(null);
+const logoMoving = ref(false);
+function imgMove() {
+  if (logoMoving.value) return;
+  logoMoving.value = true;
+  const logoAnimation = $anime
+    .timeline({
+      targets: logoImg.value,
+      delay: 400,
+      duration: 500,
+      endDelay: 400,
+      easing: 'easeInOutSine',
+      update: function (anim) {
+        console.log('logo moving : ' + Math.round(anim.progress) + '%');
+      }
+    })
+    .add({
+      translateX: 250
+    })
+    .add({
+      scale: 1.5
+    })
+    .add({
+      translateX: 0
+    })
+    .add({
+      scale: 1
+    });
 
+  logoAnimation.finished.then(logoAnimationFinished);
+
+  function logoAnimationFinished() {
+    logoMoving.value = false;
+  }
+}
+</script>
 <script>
 export default {
-  name: 'MainIndex'
+  name: 'MainIndex',
+  methods: {}
 };
 </script>
